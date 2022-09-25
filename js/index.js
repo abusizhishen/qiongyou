c4859479306ef46a5c42e35d30bf0f3ab1402fe4
abusizhishen <abusizhishen@gmail.com>
2022-09-25 22:48:53 +0800

    轮播图完成

--- js/index.js
+++ js/index.js
@@ -2,47 +2,52 @@ window.onload = function(){
     let leftbutton = document.getElementById("leftbutton");
     let rightbutton = document.getElementById("rightbutton");
     let ul = document.getElementById("banner-ul");
+    ul.appendChild(ul.firstChild.cloneNode(true))
+
+
     let left = 0;
     let step = parseInt(ul.children[0].offsetWidth, 10);
     let childrenLength = ul.children.length;
-    let minLeft = -(childrenLength-1)*step;
-
-    console.log("ul width: ", step, "minLeft: ", minLeft)
-
-
-    
-    var reset = function () {
-        ul.style.transition = 'left .3s ease 0s';
-    };
+    let idx = 0
 
     leftbutton.onclick = function () {
-        reset()
+        if (idx == 0){
+            ul.style.transition = "none";
+            idx = childrenLength-1
+            left = -idx*step;
+            ul.style.left = left + "px";
+
+            setTimeout(() => {
+                ul.style.transition = 'left .3s ease 0s';
+                idx--;
+                left = -idx*step;
+                ul.style.left = left + "px";
+            },0)
+        }else{
+            ul.style.transition = 'left .3s ease 0s';
+            idx--;
+            left = -idx*step;
+            ul.style.left = left + "px";
+        }
+        
         
 
-        if (left == 0){
-            left = minLeft;;
-            ul.style.transition = "";
-            ul.style.left = left + "px";
-        }else{
-            left += step;
-            ul.style.left = left + 'px';
-        }
-
-
-        console.log(left);
+        console.log(idx);
     }
 
     rightbutton.onclick = function () {
-        reset()
-        if (left == minLeft){
-            ul.style.transition = "";
-            left = 0;
-            ul.style.left = left + 'px';
-        }else{
-            left -= step
-            ul.style.left = left + 'px';
-
+        ul.style.transition = 'left .3s ease 0s';
+        idx++;
+        left = -idx*step;
+        ul.style.left = left + 'px';
+        if (idx == childrenLength-1){
+            setTimeout(() => {
+                ul.style.transition = 'none';
+                idx = 0
+                left = -idx*step;
+                ul.style.left = left + 'px';
+            }, 500);
         }
-        console.log(left)
+        console.log(idx);
     };
 }
