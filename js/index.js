613b90a3bb047209d2a81b22b4256635db8ff547
abusizhishen <abusizhishen@gmail.com>
2022-09-28 00:18:24 +0800

    width

--- js/index.js
+++ js/index.js
@@ -6,48 +6,69 @@ window.onload = function(){
 
 
     let left = 0;
-    let step = parseInt(ul.children[0].offsetWidth, 10);
+    let step = 16.667
     let childrenLength = ul.children.length;
     let idx = 0
-
+    var translateX = function(ele,percent) {
+        ele.style.transform = 'translateX(' + percent + '%)';
+    }
     leftbutton.onclick = function () {
         if (idx == 0){
-            ul.style.transition = "none";
+            ul.style.transition = 'none';
             idx = childrenLength-1
             left = -idx*step;
-            ul.style.left = left + "px";
+            translateX(ul,left)
 
             setTimeout(() => {
-                ul.style.transition = 'left .3s ease 0s';
+                ul.style.transition = 'transition: transform .5s ease 0s';
                 idx--;
                 left = -idx*step;
-                ul.style.left = left + "px";
+                translateX(ul,left)
             },0)
         }else{
-            ul.style.transition = 'left .3s ease 0s';
+            ul.style.transition = 'transition: transform .5s ease 0s';
             idx--;
             left = -idx*step;
-            ul.style.left = left + "px";
+            translateX(ul,left)
         }
         
         
-
         console.log(idx);
+        setCrycle(idx);
+
     }
 
     rightbutton.onclick = function () {
-        ul.style.transition = 'left .3s ease 0s';
+        ul.style.transition = 'transition: transform .5s ease 0s';
         idx++;
         left = -idx*step;
-        ul.style.left = left + 'px';
-        if (idx == childrenLength-1){
+        translateX(ul,left);
+        setCrycle(idx);
+        if (idx >= childrenLength-1){
             setTimeout(() => {
                 ul.style.transition = 'none';
-                idx = 0
-                left = -idx*step;
-                ul.style.left = left + 'px';
+                ul.style.transform = "none";
+                idx = 0;
+                // translateX(ul,0);
             }, 500);
         }
         console.log(idx);
+        
     };
+
+    let crycleUl = document.getElementById('little-cryclr');
+    let setCrycle = function(idx){
+        idx %=5
+        var children = crycleUl.children;
+        for (i=0;i<children.length;i++){
+            if (i!= idx){
+                // console.log('idx', idx)
+                children[i].className = '';
+            }else{
+                children[i].className = 'active';
+            }
+        }
+    };
+
+    setCrycle(idx)
 }
